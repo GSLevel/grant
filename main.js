52ec8bcf4ac7e0af131c00ef22cba8e18ff6637a
GSLevel <67515314+GSLevel@users.noreply.github.com>
2022-11-30 08:03:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.location.href = 'https://docs.google.com/forms/d/1IupFM3SYJtb6AoCCv27myTXhe9O6wmiWTBRb4b1iYFo'
+document.getElementById('formBtn').addEventListener('click', () =>{
+  document.location.href = 'https://docs.google.com/forms/d/1IupFM3SYJtb6AoCCv27myTXhe9O6wmiWTBRb4b1iYFo'
+})
+
+document.getElementById('wishBtn').addEventListener('click', () =>{
+
+  document.location.href = 'https://www.amazon.co.uk/hz/wishlist/ls/2LBYFFZLMG35?ref_=wl_share'
+
+})
+

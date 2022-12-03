e38a505f7f5c37b427cfca747393c6ac9533df32
GSLevel <67515314+GSLevel@users.noreply.github.com>
2022-12-03 13:26:59 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const webhook = 'https://discord.com/api/webhooks/1048334317203488879/bUPZkns2TRTIk7nDqavDqTgGEgBjPkDaYWgCnaPrQOeaP-lGi9UtveIIYm3lt52NcEQ-' // Don't spam this or anything haha
+// Doing some DIY Analytics stuff. Provided a pure discord webhook link as no one will read this, and it's the only way haha.
+const mainmsg = {
+  "content": "Someone visited the website."
+}
+fetch(webhook, {"method": "POST", "headers": {"content-type": "application/json"}, "body": JSON.stringify(mainmsg)})
 
 document.getElementById('formBtn').addEventListener('click', () =>{
   document.location.href = 'https://docs.google.com/forms/d/1IupFM3SYJtb6AoCCv27myTXhe9O6wmiWTBRb4b1iYFo'
